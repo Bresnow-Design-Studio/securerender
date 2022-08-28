b2cd57819e42261175d74e3511d2038c3eaa2e5d
bresnow <bresnow@me.com>
2022-08-27 20:38:29 -0400

    more
    Signed-off-by: bresnow <bresnow@me.com>

--- express.js
+++ express.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 8765 
+const port = 4444
 
 var serveIndex = require('serve-index')
 
---
+++ the/src/immutableDOM.js
@@ -0,0 +1,51 @@
+//slight tweak of https://github.com/GianlucaGuarini/immutable-dom
+ const config = {
+    attributes: true,
+    childList: true,
+    subtree: true,
+    characterData: true
+}
+
+// cache where we will store all the nodes to lock
+ const cache = new WeakMap()
+
+/**
+ * @param {HTMLElement} node - DOM node to lock
+ * @returns {function(): void} Mutation observer disconnect function
+ */
+export default function (node = document.documentElement, opts) {
+
+    let configuration = opts.config || config
+    // disconnect the observer and unlock the DOM node
+    const disconnect = () => callback.disconnect()
+    // function called to restore the old node HTML
+    const restore = () => {
+        // get the clone node
+        const clone = cache.get(node)
+        // replace the impure node target of a DOM update somewhere in the application
+        node.parentNode.replaceChild(clone, node)
+        // update the disconnect function
+        callback.disconnect = lock(clone)
+    }
+    // callback called on any node change
+    const callback = () => {
+        // stop the observer
+        disconnect()
+        // restore asynchronously the old HTML
+        requestAnimationFrame(restore)
+    }
+
+    // store the DOM node received into the cache
+    // we clone it deep with its children in order to keep it pristine
+    cache.set(node, node.cloneNode(true))
+
+    // instantiate the mutation observer
+    const observer = new MutationObserver(callback)
+
+    // start observing the changes
+    observer.observe(node, configuration)
+    // store the disconnect method as static method of the callback function
+    callback.disconnect = observer.disconnect.bind(observer)
+
+    return disconnect
+}
